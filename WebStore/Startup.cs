281311aee78fdf9adbd2bc5f44504d15c13ad90b
Kiruhin <ikiruhin@bk.ru>
2020-10-05 22:20:33 +0300

    Занятие 1: работа с конфигурацией

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,13 @@ namespace WebStore
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,13 +34,16 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            var hello = _configuration["CustomHelloWorld"];
+            //var logLevel = _configuration["Logging:LogLevel:Default"];
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(hello);
                 });
             });
         }
