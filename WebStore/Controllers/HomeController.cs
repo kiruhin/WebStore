dc7196136d370d8809f85e1e90bad81d88afad14
Kiruhin <ikiruhin@bk.ru>
2020-10-13 11:57:22 +0300

    ДЗ 2: недостающие представления

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -33,5 +33,46 @@ namespace WebStore.Controllers
         {
             return View();
         }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
     }
 }
