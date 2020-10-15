34a7ba3745e4af1c478131e5c36f98de58956b49
Kiruhin <ikiruhin@bk.ru>
2020-10-15 22:24:09 +0300

    Занятие 4: рефакторинг (добавили CatalogController)

---
+++ WebStore/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
