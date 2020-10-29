8aeb40083598a2d71e56c8aa1df5aacab40c5177
Kiruhin <ikiruhin@bk.ru>
2020-10-29 22:34:53 +0300

    Заняние 8: личный кабинет пользователя с историей заказов

---
+++ WebStore/Controllers/ProfileController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly IOrdersService _ordersService;
+
+        public ProfileController(IOrdersService ordersService)
+        {
+            _ordersService = ordersService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Orders()
+        {
+            var orders = _ordersService.GetUserOrders(User.Identity.Name);
+
+            var orderModels = new List<UserOrderViewModel>(orders.Count());
+
+            foreach (var order in orders)
+            {
+                orderModels.Add(new UserOrderViewModel()
+                {
+                    Id = order.Id,
+                    Name = order.Name,
+                    Address = order.Address,
+                    Phone = order.Phone,
+                    TotalSum = order.OrderItems.Sum(o => o.Price * o.Quantity)
+                });
+            }
+
+            return View(orderModels);
+        }
+
+    }
+}
